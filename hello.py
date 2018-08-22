cdb88acedab2f4cee279af6dafb84d2842fe9bc4
Yasin Çidem <yasincidem@gmail.com>
2018-08-23 00:08:52 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ def extractFrames(url):
             break
     return nframes
 
-def addProgressbarToExtractedFramesTop(nFrames, outFilename, r, g, b, thickness):
+def addProgressbarToTopLeftToRight(nFrames, outFilename, r, g, b, thickness):
     for i in range(nFrames ):
         # frame = frame.convert("RGBA")
         frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
@@ -41,9 +41,25 @@ def addProgressbarToExtractedFramesTop(nFrames, outFilename, r, g, b, thickness)
                     frame.putpixel((x_pixel, y_pixel), (int(r), int(g), int(b)))
 
         listOfImages.append(frame)
-    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, append_images=listOfImages )
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
 
-def addProgressbarToExtractedFramesBottom(nFrames, outFilename, r, g, b, thickness):
+def addProgressbarToTopRightToLeft(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for x_pixel in range(int(width / (nFrames - 1) * i )):
+                for y_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1, y_pixel), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for x_pixel in range(width):  # for every pixel:
+                for y_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1,y_pixel), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+def addProgressbarToBottomLeftToRight(nFrames, outFilename, r, g, b, thickness):
     for i in range(nFrames ):
         # frame = frame.convert("RGBA")
         frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
@@ -57,7 +73,88 @@ def addProgressbarToExtractedFramesBottom(nFrames, outFilename, r, g, b, thickne
                     frame.putpixel((x_pixel, height - y_pixel - 1), (int(r), int(g), int(b)))
 
         listOfImages.append(frame)
-    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, append_images=listOfImages )
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+def addProgressbarToBottomRightToLeft(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for x_pixel in range(int(width / (nFrames - 1) * i )):
+                for y_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1, height - y_pixel - 1), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for x_pixel in range(width):  # for every pixel:
+                for y_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1, height - y_pixel - 1), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+def addProgressbarToLeftTopToBottom(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for y_pixel in range(int(height / (nFrames - 1) * i )):
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((x_pixel,y_pixel), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for y_pixel in range(height):  # for every pixel:
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((x_pixel, y_pixel), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+def addProgressbarToLeftBottomToTop(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for y_pixel in range(int(height / (nFrames - 1) * i )):
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((x_pixel,  height - y_pixel - 1), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for y_pixel in range(height):  # for every pixel:
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel(( x_pixel, height - y_pixel - 1), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+
+def addProgressbarToRightTopToBottom(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for y_pixel in range(int(height / (nFrames - 1) * i )):
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1,y_pixel), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for y_pixel in range(height):  # for every pixel:
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1, y_pixel), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
+
+def addProgressbarToRightBottomToTop(nFrames, outFilename, r, g, b, thickness):
+    for i in range(nFrames ):
+        # frame = frame.convert("RGBA")
+        frame = Image.fromarray(numpy.uint8(extractedImagesNumpyArr[i]))
+        if i is not 0:
+            for y_pixel in range(int(height / (nFrames - 1) * i )):
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1, height - y_pixel - 1), (int(r), int(g), int(b)))
+        if i is nFrames - 1:
+            for y_pixel in range(height):  # for every pixel:
+                for x_pixel in range(int(thickness)):
+                    frame.putpixel((width - x_pixel - 1,  height - y_pixel - 1), (int(r), int(g), int(b)))
+
+        listOfImages.append(frame)
+    Image.new(mode='RGBA', size=(width, height)).save(outFilename, save_all=True, loop=0, duration=75, append_images=listOfImages )
 
 
 @app.route('/makegiffromurl')
@@ -68,13 +165,27 @@ def hello_world():
     b = request.args.get('b')
     position = request.args.get('position')
     tickness = request.args.get('thickness')
+    direction = request.args.get('direction')
     id = shortuuid.uuid()
     nFrames = extractFrames(url)
     file_path = 'output/{id}.gif'.format(id=id)
-    if position == 'top':
-        addProgressbarToExtractedFramesTop(nFrames, file_path, r, g, b, tickness)
-    if position == 'bottom':
-        addProgressbarToExtractedFramesBottom(nFrames, file_path, r, g, b, tickness)
+    if position == 'top' and direction == 'lefttoright':
+        addProgressbarToTopLeftToRight(nFrames, file_path, r, g, b, tickness)
+    if position == 'top' and direction == 'righttoleft':
+        addProgressbarToTopRightToLeft(nFrames, file_path, r, g, b, tickness)
+    if position == 'bottom' and direction == 'lefttoright':
+        addProgressbarToBottomLeftToRight(nFrames, file_path, r, g, b, tickness)
+    if position == 'bottom' and direction == 'righttoleft':
+        addProgressbarToBottomRightToLeft(nFrames, file_path, r, g, b, tickness)
+    if position == 'left' and direction == 'toptobottom':
+        addProgressbarToLeftTopToBottom(nFrames, file_path, r, g, b, tickness)
+    if position == 'left' and direction == 'bottomtotop':
+        addProgressbarToLeftBottomToTop(nFrames, file_path, r, g, b, tickness)
+    if position == 'right' and direction == 'toptobottom':
+        addProgressbarToRightTopToBottom(nFrames, file_path, r, g, b, tickness)
+    if position == 'right' and direction == 'bottomtotop':
+        addProgressbarToRightBottomToTop(nFrames, file_path, r, g, b, tickness)
+
     listOfImages[:] = []
     extractedImagesNumpyArr[:] = []
     width = 0
